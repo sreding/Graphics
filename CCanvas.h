8a3c611d56cbfba1b097a2490a72ee03ac45598a
Alexander Camenzind <camenal@usi.ch>
2017-12-02 17:29:35 +0100

    added ccavas.h back

---
+++ CCanvas.h
@@ -0,0 +1,71 @@
+/************************************************************************/
+/* Guards                                                               */
+/************************************************************************/
+#ifndef CCANVAS_H
+#define CCANVAS_H
+
+#include <iostream>
+#include <QtOpenGL>
+#include <QGLWidget>
+#include <QTimer>
+
+#include "Base.h"
+#include "texture.hpp"
+
+#include "ObjModel.h"
+#include "PlyModel.h"
+
+using namespace std;
+
+/************************************************************************/
+/* Canvas to draw                                                       */
+/************************************************************************/
+class CCanvas : public QGLWidget
+{
+  Q_OBJECT
+
+public:
+    explicit CCanvas(QWidget *parent = 0) : QGLWidget(parent),
+        textureTrain("/Users/alexandercamenzind/Desktop/group_homework/Graphics/objects/train.jpg")
+    {
+        QTimer *timer = new QTimer(this);
+        connect(timer, SIGNAL(timeout()), this, SLOT(updateGL()));
+        timer->start(10);
+    }
+    const char * pathToFile = "/Users/alexandercamenzind/Desktop/group_homework/Graphics/objects/plane.obj";
+
+protected:
+    void initializeGL();
+    void resizeGL(int width, int height);
+    void paintGL();
+
+private:
+    void lookAt(const GLdouble eyex,
+                const GLdouble eyey,
+                const GLdouble eyez,
+                const GLdouble centerx,
+                const GLdouble centery,
+                const GLdouble centerz,
+                const GLdouble upx,
+                const GLdouble upy,
+                const GLdouble upz);
+
+    void glPerspective(const GLdouble fovy,
+                       const GLdouble aspect,
+                       const GLdouble zNear,
+                       const GLdouble zFar);
+
+
+    enum View {
+        Perspective = 0,    // View the scene from a perspective (from above, from a side, or whatever)
+        Cockpit             // View the scene from the train cockpit (if you want, or whatever other view)
+    };
+
+    void setView(View _view);
+
+    // Models and textures
+    Texture textureTrain;
+
+};
+
+#endif 
