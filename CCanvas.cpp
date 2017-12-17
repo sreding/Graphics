7691f9a54e8999f344963258270369b1db41eef6
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2017-12-17 19:04:57 +0100

    refactored code

--- CCanvas.cpp
+++ CCanvas.cpp
@@ -5,194 +5,45 @@
 #include "ObjectGroup.h"
 #include "objloader.hpp"
 using namespace std;
+
 static ObjectGroup scene("");
 static ObjectGroup plane("");
 static ObjectGroup Skybox("");
 static CCanvas::CamView camView;
+static bool cockpit = false;
 //-----------------------------------------------------------------------------
 Point3d camera_position(0.0f, 0.0f, 0.0f);
 Point3d camera_direction(0.0f,0.0f,-1.0f);
-void CCanvas::initializeGL()
-{
-  glClearColor(0.0f, 0.8f, 0.8f, 0.5f);			   // black background
-  glClearDepth(1.0f);								   // depth buffer setup
-  glEnable(GL_DEPTH_TEST);						   // enables depth testing
-  glDepthFunc(GL_LEQUAL);							   // the type of depth testing to do
-  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); // really nice perspective calculations
-  glShadeModel(GL_SMOOTH);
+Point2d lastPos(0.0f,0.0f);
+bool delta_defined = false;
+GLfloat cockpit_view[] = {0, 0, 0};
+//-----------------------------------------------------------------------------
 
-  // One light source
-  glEnable(GL_LIGHTING);
-
-  glEnable(GL_LIGHT0);
-  /*
-  * The position is transformed by the modelview matrix when glLightfv is called (just as if it were
-  * a point), and it is stored in eye coordinates. If the w component of the position is 0.0,
-  * the light is treated as a directional source. Diffuse and specular lighting calculations take
-  * the light's direction, but not its actual position, into account, and attenuation is disabled.
-  * Otherwise, diffuse and specular lighting calculations are based on the actual location of the
-  * light in eye coordinates, and attenuation is enabled. The default position is (0,0,1,0); thus,
-  * the default light source is directional, parallel to, and in the direction of the -z axis.
-  */
-
-  GLfloat ambient[] =  { 0.3, 0.3, 0.3, 0.0 };
-  GLfloat diffuse[] =  { 1.0, 1.0, 1.0, 0.0 };
-  GLfloat specular[] = { 1.0, 1.0, 1.0, 0.0 };
-  glLightfv( GL_LIGHT0, GL_AMBIENT, ambient );
-  glLightfv( GL_LIGHT0, GL_DIFFUSE, diffuse );
-  glLightfv( GL_LIGHT0, GL_SPECULAR, specular );
-
-  loadOBJ(pathToFile[0],scene);
-  loadOBJ(pathToFile[1],plane);
-//  loadOBJ(pathToFile[2],plane);
-
-  /*
-  * Before you can use the texture you need to initialize it by calling the setTexture() method.
-  * Before you can use OBJ/PLY model, you need to initialize it by calling init() method.
-  */
+void CCanvas::initializeGL(){
+  initeverything();
   texturePlane.setTexture();
   textureGrass.setTexture();
   textureMountain.setTexture();
   textureWater.setTexture();
   textureLane.setTexture();
   textureTree.setTexture();
-
-  //    CCanvas::rotateScene = false;
-  grabKeyboard();
+  loadOBJ(pathToFile[0],scene);
+  loadOBJ(pathToFile[1],plane);
   camView = Still;
+  grabKeyboard();
 }
-
 //-----------------------------------------------------------------------------
 
-void CCanvas::glPerspective(const GLdouble fovy, const GLdouble aspect, const GLdouble zNear, const GLdouble zFar)
-{
-  const GLdouble d = 1.0 / tan(fovy / 360.0 * PI);
-  const GLdouble delta = zNear - zFar;
-
-  GLdouble *mat = new GLdouble[16];
-
-  mat[0] = d / aspect;
-  mat[1] = 0.0;
-  mat[2] = 0.0;
-  mat[3] = 0.0;
-
-  mat[4] = 0.0;
-  mat[5] = d;
-  mat[6] = 0.0;
-  mat[7] = 0.0;
-
-  mat[8]  = 0.0;
-  mat[9]  = 0.0;
-  mat[10] = (zNear + zFar) / delta;
-  mat[11] = -1.0;
-
-  mat[12] = 0.0;
-  mat[13] = 0.0;
-  mat[14] = 2.0 * zNear * zFar / delta;
-  mat[15] = 0.0;
-
-  glMultMatrixd(mat);
-
-  delete[] mat;
-}
-
-void CCanvas::lookAt(const GLdouble eyeX,
-  const GLdouble eyeY,
-  const GLdouble eyeZ,
-  const GLdouble centerX,
-  const GLdouble centerY,
-  const GLdouble centerZ,
-  const GLdouble upX,
-  const GLdouble upY,
-  const GLdouble upZ)
-  {
-    Point3d VP(eyeX, eyeY, eyeZ);
-    Point3d q(centerX, centerY, centerZ);
-    Point3d VUP(upX, upY, upZ);
-    Point3d VPN = VP - q;
-
-    GLdouble *mat = new GLdouble[16]; // remember: column-major order!
-
-    // set up the LookAt matrix correctly!
-    Point3d p_prime = VP;
-    Point3d z_prime = VPN.normalized();
-    Point3d x_prime = (VUP ^ z_prime).normalized();
-    Point3d y_prime = z_prime ^ x_prime;
-
-    // column 1
-    mat[0] = x_prime[0];
-    mat[1] = y_prime[0];
-    mat[2] = z_prime[0];
-    mat[3] = 0;
-
-    // column 2
-    mat[4] = x_prime[1];
-    mat[5] = y_prime[1];
-    mat[6] = z_prime[1];
-    mat[7] = 0;
-
-    // column 3
-    mat[8] = x_prime[2];
-    mat[9] = y_prime[2];
-    mat[10] = z_prime[2];
-    mat[11] = 0;
-
-    // sure? column 4
-    mat[12] = -x_prime * p_prime;
-    mat[13] = -y_prime * p_prime;
-    mat[14] = -z_prime * p_prime;
-    mat[15] = 1;
-
-    glMultMatrixd(mat);
-
-    delete[] mat;
-  }
-
-  void CCanvas::resizeGL(int width, int height)
-  {
-    // set up the window-to-viewport transformation
-    glViewport(0, 0, width, height);
-
-    // vertical camera opening angle
-    double beta = 80.0;
-
-    // aspect ratio
-    double gamma;
-    if(height > 0) gamma = width / (double)height;
-    else gamma = width;
-
-    // front and back clipping plane at
-    double n = -0.01;
-    double f = -1000.0;
-
-    // frustum corners
-    // double t = -tan(beta * 3.14159 / 360.0) * n;
-    // double b = -t;
-    // double r = gamma * t;
-    // double l = -r;
-
-    // set projection matrix
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    // glFrustum(l,r, b,t, -n,-f);
-
-    // alternatively, directly from alpha and gamma
-    glPerspective(beta, gamma, -n, -f);
-  }
-
-  //-----------------------------------------------------------------------------
-  Point2d lastPos(0.0f,0.0f);
-  bool delta_defined = false;
-
-  bool CCanvas::event(QEvent *event){
-    float moveSpeed = 0.2;
-    if (event->type() == QEvent::KeyPress) {
+bool CCanvas::event(QEvent *event){
+  float moveSpeed = 0.2;
+  switch (event->type()) {
+    case QEvent::KeyPress : {
       QKeyEvent *ke = static_cast<QKeyEvent *>(event);
       if (ke->key() == Qt::Key_R) {
-        qDebug() << "R";
-        camView = CCanvas::Rotate;
-        //            CCanvas::camView = Rotate;
-        //            rotateScene = !rotateScene;
+        // camView = CCanvas::Rotate;
+        cockpit = true;
+        // CCanvas::camView = Rotate;
+        // rotateScene = !rotateScene;
         return true;
       }else if(ke->key() == Qt::Key_Right || ke->key() == Qt::Key_D ){
         camera_position[0]-=0.2;
@@ -200,11 +51,13 @@ void CCanvas::lookAt(const GLdouble eyeX,
         camera_position[0]+=0.2;
       }else if(ke->key() == Qt::Key_Up || ke->key() == Qt::Key_W ){
         camera_position += camera_direction.normalized()*moveSpeed;
-        //            camera_position[2]-=0.2;
       }else if(ke->key() == Qt::Key_Down || ke->key() == Qt::Key_S ){
         camera_position -= camera_direction.normalized()*moveSpeed;
       }
-    }else if(event->type() == QEvent::MouseMove){
+      break;
+    }
+
+    case QEvent::MouseMove : {
       QMouseEvent *me = static_cast<QMouseEvent *>(event);
       if(!delta_defined){
         delta_defined = true;
@@ -219,15 +72,16 @@ void CCanvas::lookAt(const GLdouble eyeX,
         rotatePointX(&camera_direction,-dy*speed);
         //            camera_direction.y() -= dy*speed;
       }
-
-    }else if(event->type() == QEvent::MouseButtonRelease){
-      delta_defined = false;
+      break;
     }
 
+    case QEvent::MouseButtonRelease :{
+      delta_defined = false;
+      break;
+    }
+  }
     return QWidget::event(event);
   }
-
-
 
   void setmainaxis(float x, float y){
     static float distance_camera = -25;
@@ -245,11 +99,6 @@ void CCanvas::lookAt(const GLdouble eyeX,
     glScalef(scaleFactor,scaleFactor,scaleFactor);
     glRotatef(90 + axis_rotate, 0.0f, 1.0f, 0.0f);
   }
-
-
-  GLfloat modelview_matrix[] = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
-
-
 
   void CCanvas::setView(View _view) {
     static bool tookoff =false;
@@ -269,14 +118,12 @@ void CCanvas::lookAt(const GLdouble eyeX,
 
     switch(_view) {
       case Axis:
-      setmainaxis(x, y);
-      break;
+        setmainaxis(x, y);
+        break;
 
       case Main_Body: {
         switch(path[plane_state]){
           case Start: {
-            x+=acc;
-            acc *=1.0075;
             if(x <= 1){
               y = 0.2;
             }else{
@@ -291,12 +138,14 @@ void CCanvas::lookAt(const GLdouble eyeX,
 
             glTranslatef(x,y,z);
             static float rotate_show = 0.0;
-//            rotate_show += 0.2;
+            // rotate_show += 0.2;
             glRotatef(270, 0.0,1.0,0.0);
             glRotatef(15 + rotate_show, 1.0,0.0,0.0);
+            x+=acc;
+            acc *=1.0075;
             break;
           }
-          case Land:{// needs more
+          case Land:{
             x+=acc;
             acc *=0.9;
             if(x <= 1){
@@ -325,138 +174,118 @@ void CCanvas::lookAt(const GLdouble eyeX,
             curve += 0.2;
             angle += 0.1;
           }
-          // TODO
-//                    glRotatef(curve*9,4.0,0.0,0.0);
-//                    y =   x * x * 0.05;
-//                    Translate(x,y, z); // put in the axis
-//                    gRotate(angle *9,-1.0,1.0,1.0);
-//                    gRotate(270, 0.0,1.0,0.0);
-//                    gRotate(15, 1.0,0.0,0.0);
-
+          glTranslatef(x,y, z); // put in the axis
+          glRotated(angle *9,-1.0,1.0,1.0);
+          glRotated(270, 0.0,1.0,0.0);
+          glRotated(15, 1.0,0.0,0.0);
           break;
           case Circle:
-          if(check){
-            centerz = -z-radius;
-            centerx = -x;
-            acc = 1.5;
-            check = false;
-          }
-
-          x = -(centerx + (radius * cos(acc)));
-          z = (centerz + (radius * sin(acc)));
-          if(reached_max && curve > 0.0){
-            //                        curve -=0.2;
-            angle += 0.016;
-
-          }else if(curve > 10){
-            reached_max = true;
-          }else if(curve < 0.0){
-            reached_max = false;
-            curve = 0.2;
-            //set next path
-            plane_state = 0;
-          }else{
-            curve += 0.05;
-            angle += 0.016;
-
-          }
-          acc += 0.01;
-            // TODO
-          //                    y =   x * x * 0.05;
-          //                    Translate(x,y, z); // put in the axis
-          //                    gRotate(270, 0.0,1.0,0.0);
-          //                    gRotate(angle *36,0.0,1.0,0.0);
-          //                    gRotate(curve*4.5,0.0,0.0,1.0);
-          //                    gRotate(15, 1.0,0.0,0.0);
-
-          break;
+            if(check){
+              centerz = -z-radius;
+              centerx = -x;
+              acc = 1.5;
+              check = false;
+            }
+            x = -(centerx + (radius * cos(acc)));
+            z = (centerz + (radius * sin(acc)));
+            if(reached_max && curve > 0.0){
+              // curve -=0.2;
+              angle += 0.016;
+            }else if(curve > 10){
+              reached_max = true;
+            }else if(curve < 0.0){
+              reached_max = false;
+              curve = 0.2;
+              //set next path
+              plane_state = 0;
+            }else{
+              curve += 0.05;
+              angle += 0.016;
+            }
+            acc += 0.01;
+            glTranslatef(x,y, z); // put in the axis
+            glRotated(270, 0.0,1.0,0.0);
+            glRotated(angle *36,0.0,1.0,0.0);
+            glRotatef(curve*4.5,0.0,0.0,1.0);
+            glRotated(15, 1.0,0.0,0.0);
+            break;
           case Right:
-          x--;
-          break;
+            x--;
+            break;
           case Up:
-          y++;
-          break;
+            y++;
+            break;
           case Down:
-          y--;
-          break;
-
+            y--;
+            break;
         }
-
         glScaled(0.1,0.1,0.1);
         break;
       }
 
       case Propeller:
-      rotatepropeller(acc);
-      break;
+        rotatepropeller(acc);
+        break;
 
       case L_Wheel:
-      static float l_wheel = 3.4;
-      if(l_wheel > 0.0 && tookoff)
-      l_wheel -= 0.05;
-      glTranslated(-2.2,-0.3,-1.6); // l wheel
-      glRotated(-5, 0.0,1.0,0.0);
-      glRotated(cos(l_wheel) * 50 + 33, 0.0,0.0,1.0);
-      break;
+        static float l_wheel = 3.4;
+        if(l_wheel > 0.0 && tookoff)
+          l_wheel -= 0.05;
+        glTranslated(-2.2,-0.3,-1.6); // l wheel
+        glRotated(-5, 0.0,1.0,0.0);
+        glRotated(cos(l_wheel) * 50 + 33, 0.0,0.0,1.0);
+        break;
 
       case R_Wheel:
-      static float r_wheel = 3.4;
-      if(r_wheel > 0.0 && tookoff)
-      r_wheel -= 0.05;
-      glTranslated(2.2,-0.3,-1.6); // r wheel
-      glRotated(5, 0.0,1.0,0.0);
-      glRotated(-cos(r_wheel) * 50 - 33, 0.0,0.0,1.0);
-      break;
+        static float r_wheel = 3.4;
+        if(r_wheel > 0.0 && tookoff)
+          r_wheel -= 0.05;
+        glTranslated(2.2,-0.3,-1.6); // r wheel
+        glRotated(5, 0.0,1.0,0.0);
+        glRotated(-cos(r_wheel) * 50 - 33, 0.0,0.0,1.0);
+        break;
 
       case L_Flap:
-      static float l_flap = 0.0;
-      l_flap += 0.05;
-      glTranslated(-4.0,-0.1,-0.6); // l flap
-      glRotated(-7.0,0.0,1.0,0.0);
-      glRotated(-8,0.0,0.0,1.0);
-      //            glRotated(cos(l_flap) * 60 - 30, 1.0,0.0,0.0);
-      break;
+        static float l_flap = 0.0;
+        l_flap += 0.05;
+        glTranslated(-4.0,-0.1,-0.6); // l flap
+        glRotated(-7.0,0.0,1.0,0.0);
+        glRotated(-8,0.0,0.0,1.0);
+        //            glRotated(cos(l_flap) * 60 - 30, 1.0,0.0,0.0);
+        break;
 
       case R_Flap:
-      static float r_flap = 0.0;
-      r_flap += 0.05;
-      glTranslated(4.0,-0.1,-0.6); // r flap
-      glRotated(7.0,0.0,1.0,0.0);
-      glRotated(8,0.0,0.0,1.0);
-      //            glRotated(cos(r_flap) * 60 - 30, 1.0,0.0,0.0);
-      break;
+        static float r_flap = 0.0;
+        r_flap += 0.05;
+        glTranslated(4.0,-0.1,-0.6); // r flap
+        glRotated(7.0,0.0,1.0,0.0);
+        glRotated(8,0.0,0.0,1.0);
+        //            glRotated(cos(r_flap) * 60 - 30, 1.0,0.0,0.0);
+        break;
 
       case Back:
-      static float back = 0.0;
-      back += 0.05;
-      glTranslated(-0.0,0.25,4.55); // back
-      //            glRotated(-cos(back) * 60 , 0.0,1.0,0.0);
-      glRotated(3, 1.0,0.0,0.0);
-      break;
+        static float back = 0.0;
+        back += 0.05;
+        glTranslated(-0.0,0.25,4.55); // back
+        //            glRotated(-cos(back) * 60 , 0.0,1.0,0.0);
+        glRotated(3, 1.0,0.0,0.0);
+        break;
 
       case R_B_Flap:
-      static float r_b_flap = 0.0;
-      r_b_flap += 0.05;
-      glTranslated(0.95,0.0,4.0); // r back flap
-      //            glRotated(cos(r_b_flap) * 60 - 30, 1.0,0.0,0.0);
-      break;
+        static float r_b_flap = 0.0;
+        r_b_flap += 0.05;
+        glTranslated(0.95,0.0,4.0); // r back flap
+        //            glRotated(cos(r_b_flap) * 60 - 30, 1.0,0.0,0.0);
+        break;
 
       case L_B_Flap:
-      static float l_b_flap = 0.0;
-      l_b_flap += 0.05;
-      glTranslated(-0.95,0.0,4.0); // l back flap
-      //            glRotated(cos(l_b_flap) * 60 - 30, 1.0,0.0,0.0);
-      break;
-
+        static float l_b_flap = 0.0;
+        l_b_flap += 0.05;
+        glTranslated(-0.95,0.0,4.0); // l back flap
+        //            glRotated(cos(l_b_flap) * 60 - 30, 1.0,0.0,0.0);
+        break;
     }
   }
-
-  void popandpush(){
-    glPopMatrix(); // IDENTITY AXIS MAIN_BODY
-    glPushMatrix(); // IDENTITY AXIS MAIN_BODY MAIN_BODY
-  }
-
-
 
   void CCanvas::free_camera_lookat(){
     lookAt(camera_position[0],camera_position[1],camera_position[2],
@@ -466,27 +295,23 @@ void CCanvas::lookAt(const GLdouble eyeX,
     }
 
   void updatemodelview(float mod[]){
-      modelview_matrix[12] += mod[12];
-      modelview_matrix[13] += mod[13] + 0.3;
-      modelview_matrix[14] += mod[14];
+//      Point3d a(0,0,-1);
+//      rotatePointX(&a, 45);
+//      cout << round(a.x()) << " " << round(a.y()) << " "  << round(a.z()) << endl;
+      cockpit_view[0] += mod[12];
+      cockpit_view[1] += mod[13] + 0.3;
+      cockpit_view[2] += mod[14];
   }
 
     void CCanvas::paintGL()
     {
-      // clear screen and depth buffer
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-      // set model-view matrix
-      glMatrixMode(GL_MODELVIEW);
-      glEnable( GL_NORMALIZE );
-      glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-      glEnable(GL_COLOR_MATERIAL);
-      glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
+      initpaintgl();
       glPushMatrix(); // IDENTITY IDENTITY
       glLoadIdentity();
 //      free_camera_lookat();
-      lookAt(modelview_matrix[12], modelview_matrix[13], modelview_matrix[14], modelview_matrix[12], modelview_matrix[13], modelview_matrix[14] -1  ,  0,1,0);
+      if(cockpit){
+          lookAt(cockpit_view[0], cockpit_view[1], cockpit_view[2], cockpit_view[0], cockpit_view[1], cockpit_view[2] -1,  0,1,0);
+      }
 
       //    ##################AXES##################
       setView(View::Axis);
@@ -495,20 +320,13 @@ void CCanvas::lookAt(const GLdouble eyeX,
       glEnable(GL_LIGHTING);
 
       // ##################AIRPLANE##################
-      GLfloat amb[]  = {1.0f, 1.0f, 1.0f};
-      GLfloat diff[] = {0.7f, 0.7f, 0.7f};
-      GLfloat spec[] = {0.1f, 0.1f, 0.1f};
-      GLfloat shin = 0.0001;
-      glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, amb);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diff);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, spec);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, &shin);
-      glColor3f(1.0f, 1.0f, 1.0f);
+      setplanematerial();
 
       texturePlane.bind();
       setView(View::Main_Body);
       plane.objects[0].draw();
       glPushMatrix(); // IDENTITY AXIS MAIN_BODY MAIN_BODY
+
       float model[16];
       glGetFloatv (GL_MODELVIEW, model);
       updatemodelview(model);
--- genericmethods.cpp
+++ genericmethods.cpp
@@ -1,6 +1,127 @@
 #include "CCanvas.h"
 #include "genericmethods.h"
 
+void CCanvas::resizeGL(int width, int height)
+{
+  // set up the window-to-viewport transformation
+  glViewport(0, 0, width, height);
+
+  // vertical camera opening angle
+  double beta = 80.0;
+
+  // aspect ratio
+  double gamma;
+  if(height > 0) gamma = width / (double)height;
+  else gamma = width;
+
+  // front and back clipping plane at
+  double n = -0.01;
+  double f = -1000.0;
+
+  // frustum corners
+  // double t = -tan(beta * 3.14159 / 360.0) * n;
+  // double b = -t;
+  // double r = gamma * t;
+  // double l = -r;
+
+  // set projection matrix
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  // glFrustum(l,r, b,t, -n,-f);
+
+  // alternatively, directly from alpha and gamma
+  glPerspective(beta, gamma, -n, -f);
+}
+
+void CCanvas::lookAt(const GLdouble eyeX,
+  const GLdouble eyeY,
+  const GLdouble eyeZ,
+  const GLdouble centerX,
+  const GLdouble centerY,
+  const GLdouble centerZ,
+  const GLdouble upX,
+  const GLdouble upY,
+  const GLdouble upZ)
+  {
+    Point3d VP(eyeX, eyeY, eyeZ);
+    Point3d q(centerX, centerY, centerZ);
+    Point3d VUP(upX, upY, upZ);
+    Point3d VPN = VP - q;
+
+    GLdouble *mat = new GLdouble[16]; // remember: column-major order!
+
+    // set up the LookAt matrix correctly!
+    Point3d p_prime = VP;
+    Point3d z_prime = VPN.normalized();
+    Point3d x_prime = (VUP ^ z_prime).normalized();
+    Point3d y_prime = z_prime ^ x_prime;
+
+    // column 1
+    mat[0] = x_prime[0];
+    mat[1] = y_prime[0];
+    mat[2] = z_prime[0];
+    mat[3] = 0;
+
+    // column 2
+    mat[4] = x_prime[1];
+    mat[5] = y_prime[1];
+    mat[6] = z_prime[1];
+    mat[7] = 0;
+
+    // column 3
+    mat[8] = x_prime[2];
+    mat[9] = y_prime[2];
+    mat[10] = z_prime[2];
+    mat[11] = 0;
+
+    // sure? column 4
+    mat[12] = -x_prime * p_prime;
+    mat[13] = -y_prime * p_prime;
+    mat[14] = -z_prime * p_prime;
+    mat[15] = 1;
+
+    glMultMatrixd(mat);
+
+    delete[] mat;
+}
+
+void CCanvas::glPerspective(const GLdouble fovy, const GLdouble aspect, const GLdouble zNear, const GLdouble zFar)
+{
+  const GLdouble d = 1.0 / tan(fovy / 360.0 * PI);
+  const GLdouble delta = zNear - zFar;
+
+  GLdouble *mat = new GLdouble[16];
+
+  mat[0] = d / aspect;
+  mat[1] = 0.0;
+  mat[2] = 0.0;
+  mat[3] = 0.0;
+
+  mat[4] = 0.0;
+  mat[5] = d;
+  mat[6] = 0.0;
+  mat[7] = 0.0;
+
+  mat[8]  = 0.0;
+  mat[9]  = 0.0;
+  mat[10] = (zNear + zFar) / delta;
+  mat[11] = -1.0;
+
+  mat[12] = 0.0;
+  mat[13] = 0.0;
+  mat[14] = 2.0 * zNear * zFar / delta;
+  mat[15] = 0.0;
+
+  glMultMatrixd(mat);
+
+  delete[] mat;
+}
+
+void popandpush(){
+  glPopMatrix(); // IDENTITY AXIS MAIN_BODY
+  glPushMatrix(); // IDENTITY AXIS MAIN_BODY MAIN_BODY
+}
+
 void rotatePointX(Point3d *p, float alpha){
   float y = cos(alpha)*p->y() - sin(alpha)*p->z();
   float z = sin(alpha)*p->y()+ cos(alpha)*p->z();
@@ -64,4 +185,75 @@ void showaxis(){
     glEnable(GL_LIGHTING);
     glMatrixMode(GL_MODELVIEW);
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
+void initpaintgl(){
+    // clear screen and depth buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glEnable( GL_NORMALIZE );
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+    glEnable(GL_COLOR_MATERIAL);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
+void setplanematerial(){
+    GLfloat amb[]  = {1.0f, 1.0f, 1.0f};
+    GLfloat diff[] = {0.7f, 0.7f, 0.7f};
+    GLfloat spec[] = {0.1f, 0.1f, 0.1f};
+    GLfloat shin = 0.0001;
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, amb);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diff);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, spec);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, &shin);
+//      glColor3f(1.0f, 1.0f, 1.0f);
+}
+
+void initeverything(){
+  glClearColor(0.0f, 0.8f, 0.8f, 0.5f);			   // black background
+  glClearDepth(1.0f);								   // depth buffer setup
+  glEnable(GL_DEPTH_TEST);						   // enables depth testing
+  glDepthFunc(GL_LEQUAL);							   // the type of depth testing to do
+  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); // really nice perspective calculations
+  glShadeModel(GL_SMOOTH);
+
+  // One light source
+  glEnable(GL_LIGHTING);
+
+  glEnable(GL_LIGHT0);
+  /*
+  * The position is transformed by the modelview matrix when glLightfv is called (just as if it were
+  * a point), and it is stored in eye coordinates. If the w component of the position is 0.0,
+  * the light is treated as a directional source. Diffuse and specular lighting calculations take
+  * the light's direction, but not its actual position, into account, and attenuation is disabled.
+  * Otherwise, diffuse and specular lighting calculations are based on the actual location of the
+  * light in eye coordinates, and attenuation is enabled. The default position is (0,0,1,0); thus,
+  * the default light source is directional, parallel to, and in the direction of the -z axis.
+  */
+
+  GLfloat ambient[] =  { 0.3, 0.3, 0.3, 0.0 };
+  GLfloat diffuse[] =  { 1.0, 1.0, 1.0, 0.0 };
+  GLfloat specular[] = { 1.0, 1.0, 1.0, 0.0 };
+  glLightfv( GL_LIGHT0, GL_AMBIENT, ambient );
+  glLightfv( GL_LIGHT0, GL_DIFFUSE, diffuse );
+  glLightfv( GL_LIGHT0, GL_SPECULAR, specular );
+
+  // loadOBJ(pathToFile[0],scene);
+  // loadOBJ(pathToFile[1],plane);
+//  loadOBJ(pathToFile[2],plane);
+
+  /*
+  * Before you can use the texture you need to initialize it by calling the setTexture() method.
+  * Before you can use OBJ/PLY model, you need to initialize it by calling init() method.
+  */
+  // texturePlane.setTexture();
+  // textureGrass.setTexture();
+  // textureMountain.setTexture();
+  // textureWater.setTexture();
+  // textureLane.setTexture();
+  // textureTree.setTexture();
+
+  //    CCanvas::rotateScene = false;
+  // grabKeyboard();
+  // camView = Still;
 }
