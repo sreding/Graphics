61e04555eaee99803521b09045da3d673a7009b2
sreding <redins@usi.ch>
2017-12-19 08:43:30 +0100

    smoother landing

--- CCanvas.cpp
+++ CCanvas.cpp
@@ -188,15 +188,15 @@ void CCanvas::setView(View _view) {
       glTranslatef(x,y, z); // put in the axis
       glRotated(270, 0.0,1.0,0.0);
 //      glRotated(180,0.0,1.0,0.0);
-      glRotatef(curve*4.5,0.0,0.0,1.0);
-      glRotated(15, 1.0,0.0,0.0);
+//      glRotatef(curve*4.5,0.0,0.0,1.0);
+      glRotated(curve, 1.0,0.0,0.0);
       break;
     case Left:
       glTranslatef(x,y, z); // put in the axis
       glRotated(270, 0.0,1.0,0.0);
       glRotated(angle *36,0.0,1.0,0.0);
       glRotatef(curve*4.5,0.0,0.0,1.0);
-      // glRotated(15, 1.0,0.0,0.0);
+//       glRotated(15, 1.0,0.0,0.0);
     break;
 
     case Circle:
@@ -282,6 +282,7 @@ void CCanvas::setView(View _view) {
         }
         case Land:
           if(tookoff){
+              curve = 0.0;
               tookoff = false;
           }
 //          y -= 0.01;
@@ -290,6 +291,10 @@ void CCanvas::setView(View _view) {
           if(z < 0.0){
             z += 0.001;
           }
+          if(curve < 15.0){
+             curve += 0.1;
+          }
+
           if(y <= 0.2){
             acc = 0.01;
             y = 0.2;
@@ -320,8 +325,8 @@ void CCanvas::setView(View _view) {
           glTranslatef(x,y, z); // put in the axis
           glRotated(270, 0.0,1.0,0.0);
 //          glRotated(180,0.0,1.0,0.0);
-          glRotatef(curve*4.5,0.0,0.0,1.0);
-          // glRotated(15, 1.0,0.0,0.0);
+//          glRotatef(,0.0,0.0,1.0);
+           glRotated(curve, 1.0,0.0,0.0);
           break;
         case Left:
           if(check){
@@ -355,7 +360,7 @@ void CCanvas::setView(View _view) {
           glRotated(270, 0.0,1.0,0.0);
           glRotated(angle *36,0.0,1.0,0.0);
           glRotatef(curve*4.5,0.0,0.0,1.0);
-          // glRotated(15, 1.0,0.0,0.0);
+//           glRotated(, 1.0,0.0,0.0);
         break;
 
         case Circle:
